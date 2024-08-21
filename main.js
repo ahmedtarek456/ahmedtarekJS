ad8291224e278726fc9eda0c677bae28e0b8d391
ahmedtarek456 <at583170@gmail.com>
2024-08-21 13:57:22 +0300

    Add files via upload
    Signed-off-by: ahmedtarek456 <at583170@gmail.com>

---
+++ main.js
@@ -0,0 +1,11 @@
+function showCard() {
+    var card = document.getElementById("card");
+    if (card.style.display === "none" || card.style.display === "") {
+        card.style.display = "block";
+    } else {
+        card.style.display = "none";
+    }
+    
+    
+    
+}
